d82ce3683ca05dad2602a70dc20711bc93ea917b
maryna <merry.kosiv@gmail.com>
2020-12-11 19:50:05 +0200

    initial commit

---
+++ src/data.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import { FaBehance, FaFacebook, FaLinkedin, FaTwitter, FaGit } from 'react-icons/fa'
+export const links = [
+    {
+        id: 1,
+        url: '/',
+        text: 'home',
+    },
+    {
+        id: 2,
+        url: '/about',
+        text: 'about',
+    },
+    {
+        id: 3,
+        url: '/projects',
+        text: 'profile',
+    },
+    {
+        id: 4,
+        url: '/contact',
+        text: 'projects',
+    },
+    {
+        id: 5,
+        url: '/profile',
+        text: 'blog',
+    },
+    {
+        id: 6,
+        url: '/profile',
+        text: 'contact',
+    },
+];
+
+export const social = [
+    {
+        id: 1,
+        url: 'https://www.twitter.com',
+        icon: <FaFacebook />,
+    },
+    {
+        id: 2,
+        url: 'https://www.twitter.com',
+        icon: <FaTwitter />,
+    },
+    {
+        id: 3,
+        url: 'https://www.twitter.com',
+        icon: <FaLinkedin />,
+    },
+    {
+        id: 4,
+        url: 'https://www.twitter.com',
+        icon: <FaBehance />,
+    },
+    {
+        id: 5,
+        url: 'https://www.twitter.com',
+        icon: <FaGit />,
+    },
+];
